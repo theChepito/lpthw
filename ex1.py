39378b94d8bd4b622469586d36c1819860671c75
Joseph <joseph.p.schmitt@gmail.com>
2019-04-10 22:40:52 -0700

    Complete exercise one; print

---
+++ ex1.py
@@ -0,0 +1,8 @@
+# print ("Hello World!")
+# print ("Hello Again")
+# print ("I like typing this.")
+# print ('Yay! Printing.')
+# print ("I'd much rather you 'not'.")
+print ('I "said" do not touch this.')
+print ("This is a change to see how the git staging process works")
+print ("Yet another change to test the modified status")
