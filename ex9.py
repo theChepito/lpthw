d5123ba7f67f0a97167d4176fe640d8d2ddfb28e
Joseph <joseph.p.schmitt@gmail.com>
2019-04-11 19:54:35 -0700

    Complete exercise nine, printing, printing, printing

---
+++ ex9.py
@@ -0,0 +1,17 @@
+# define the variable days to capture a single string of all the days
+days = "Mon Tue Wed Thu Fri Sat Sun"
+
+# define the variable months, with new line "n" preceding Feb - Aug
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# print statements to list all the days and months
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+# print statement using triple quotes so each line is displayed as its own lines
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
