c0a2957f5bb5983050f6a245b6e1531f25761b1b
Joseph <joseph.p.schmitt@gmail.com>
2019-04-18 19:39:57 -0700

    Complete exercise thirteen; parameters, unpacking, variables

---
+++ ex13.py
@@ -0,0 +1,14 @@
+from sys import argv
+# read the WYSS section for how to run this
+# these are arguments that we will pass when running the script
+script, one, two, th, quatro, fifth = argv
+
+print("The script is called:", script)
+print("Your first variable is:", one)
+print("Your second variable is:", two)
+print("Your third variable is:", th)
+print("Your fourth variable is:", quatro)
+print("Your fifth variable is:", fifth)
+
+# to run the argv function in command line, add the variable arguments after
+# the ex13.py in order to pass the variables to the arguments
