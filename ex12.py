e101f1fa11a80c02996c3fe1b06b74e1b7a37ccb
Joseph <joseph.p.schmitt@gmail.com>
2019-04-18 19:39:31 -0700

    Complete exercise twelve, prompting people

---
+++ ex12.py
@@ -0,0 +1,9 @@
+size = input ("How big a boy are you? ")
+
+age = input("How old are you? ")
+
+height = input("How tall are you? ")
+
+weight = input("How much do you weigh? ")
+
+print(f"So, you say you are a {weight} {size} boy, standing {height} tall.")
